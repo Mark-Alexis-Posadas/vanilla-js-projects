28f102fb8c284b6e3dd4786991560f5e181b8d13
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-03-25 15:47:33 +0800

    done accordion

---
+++ accordion/README.md
@@ -0,0 +1,34 @@
+# Accordion Project
+
+This is a simple accordion project built using HTML, CSS, and JavaScript.
+
+## Project Description
+
+The accordion project consists of a series of collapsible panels that can be expanded and collapsed by clicking on the panel headers. Each panel contains a title and some content that is hidden by default but can be revealed when the panel is expanded.
+
+The project is built using HTML to structure the panels, CSS to style the panels, and JavaScript to handle the expanding and collapsing functionality.
+
+## Installation
+
+To install the project, simply download or clone the project files from the GitHub repository. Once downloaded, open the index.html file in your browser to view the project.
+
+```bash
+git clone https://github.com/username/accordion-project.git
+```
+
+## Usage
+
+Usage
+To use the accordion project, simply click on the panel headers to expand or collapse the panels. You can also add additional panels or modify the content of the existing panels by editing the HTML code.
+
+## Contributing
+
+If you would like to contribute to the project, please fork the repository and submit a pull request with your changes. All contributions are welcome!
+
+## Credits
+
+The accordion project was created by Mark Alexis Posadas and is licensed under the MIT License.
+
+## Demo
+
+Coming soon
--- accordion/js/index.js
+++ accordion/js/index.js
@@ -12,16 +12,42 @@
 //   });
 // });
 
+// const accordionItems = document.querySelectorAll(".accordion__items");
+
+// // Add event listener to each accordion item
+// accordionItems.forEach((item) => {
+//   const header = item.querySelector(".accordion__flex");
+//   const content = item.querySelector(".accordion__content");
+//   console.log(header, content);
+
+//   // Toggle active class on header click
+//   header.addEventListener("click", () => {
+//     content.classList.toggle("accordion__content--active");
+//     header.querySelector(".fa-caret-down").classList.toggle("fa-rotate-180");
+//   });
+// });
+
 const accordionItems = document.querySelectorAll(".accordion__items");
 
 // Add event listener to each accordion item
-accordionItems.forEach((item) => {
+accordionItems.forEach((item, index) => {
   const header = item.querySelector(".accordion__flex");
   const content = item.querySelector(".accordion__content");
-  console.log(header, content);
 
   // Toggle active class on header click
   header.addEventListener("click", () => {
+    // Hide other accordion contents
+    for (let i = 0; i < accordionItems.length; i++) {
+      if (i !== index) {
+        accordionItems[i]
+          .querySelector(".accordion__content")
+          .classList.remove("accordion__content--active");
+        accordionItems[i]
+          .querySelector(".fa-caret-down")
+          .classList.remove("fa-rotate-180");
+      }
+    }
+
     content.classList.toggle("accordion__content--active");
     header.querySelector(".fa-caret-down").classList.toggle("fa-rotate-180");
   });
