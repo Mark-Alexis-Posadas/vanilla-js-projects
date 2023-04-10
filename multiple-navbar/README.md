c27fa97c3da39718d0740b9368a1972288b71d31
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-04-10 13:54:54 +0800

    done and added readme.md

---
+++ multiple-navbar/README.md
@@ -0,0 +1,27 @@
+# Multiple Tabs Using Vanilla JavaScript
+
+This project demonstrates how to create a multiple tabs interface using vanilla JavaScript.
+
+## Getting Started
+
+To use this project, simply clone the repository and open the `index.html` file in your web browser.
+
+`git clone https://github.com/<your-github-username>/multiple-tabs.git`
+cd multiple-tabs
+open index.html`
+
+## How it Works
+
+The `index.html file` contains the basic HTML structure of the tabs interface. The `css/index.css` file contains the styles for the interface, and the `js/index.js` file contains the JavaScript code that makes the tabs interactive.
+
+The JavaScript code works by selecting all the `nav` and `.content` elements on the page, and then attaching click event listeners to each `li` element in the nav element. When a `li` element is clicked, the JavaScript code removes the `active` class from all the `li` elements in the nav element, and then adds the active class to the clicked li element. It also shows the corresponding `content` element in the .content element by adding the `content-active` class to the corresponding element.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- multiple-navbar/js/index.js
+++ multiple-navbar/js/index.js
@@ -1,13 +1,11 @@
 // Select all elements with the "nav" tag
 const navbars = document.querySelectorAll("nav");
+const contents = document.querySelectorAll(".content");
 
-// Loop through each "nav" element
-navbars.forEach((navbar) => {
+// Loop through each set of "nav" and "content" elements
+for (let i = 0; i < navbars.length; i++) {
   // Find all "li" elements within the current "nav" element
-  const navLinks = navbar.querySelectorAll("li");
-
-  // Log the "li" elements to the console
-  console.log(navLinks);
+  const navLinks = navbars[i].querySelectorAll("li");
 
   // Loop through each "li" element
   navLinks.forEach((link) => {
@@ -20,9 +18,21 @@ navbars.forEach((navbar) => {
 
       // Add the "active" class to the clicked "li" element
       link.classList.add("active");
+
+      // Find the content element that corresponds to the clicked "li" element
+      const target = link.dataset.target;
+      const content = contents[i].querySelector(target);
+
+      // Hide all content elements within the current "content" element
+      contents[i].querySelectorAll(".content-items").forEach((item) => {
+        item.classList.remove("content-active");
+      });
+
+      // Show the content element that corresponds to the clicked "li" element
+      content.classList.add("content-active");
     };
 
     // Add a click event listener to the "li" element
     link.addEventListener("click", handleClick);
   });
-});
+}
